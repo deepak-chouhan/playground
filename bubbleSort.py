19ecbbd53744f75fda06bc70a0fab34f766b3962
deepak <deepakceo0@gmail.com>
2023-01-25 22:13:06 +0530

    optimed algorithm for variable arr length

--- bubbleSort.py
+++ bubbleSort.py
@@ -4,9 +4,14 @@ def bubbleSort(arr):
         return arr
     
     for i in range(n):
+        flag = False
         for j in range(n-1-i):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                flag = True
+        if flag == False:
+            break
+
     return arr
 
 arr = [8, 5, 7, 3, 2]
