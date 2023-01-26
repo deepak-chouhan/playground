0599fcd379ebc6a66d17dffbf8418ed2237c233b
deepak <deepakceo0@gmail.com>
2023-01-26 17:34:49 +0530

    added test cases

--- pallindrome.py
+++ pallindrome.py
@@ -1,15 +1,17 @@
 def checkPallindrome(word):
+    isPallindrome = True
+    
     if len(word) < 1:
         return -1
+    elif len(word) == 1:
+        return isPallindrome
     
-    isPallindrome = True
     start = 0
     end = len(word) - 1
     while start < end:
         if word[start] != word[end]:
             isPallindrome = False
             break
-        
         start += 1
         end -= 1
     return isPallindrome
